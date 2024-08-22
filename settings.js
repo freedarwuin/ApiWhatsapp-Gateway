e376adacc0c9b7f25898eff0d71df185b002a167
ElGuason <darwuinjose@gmail.com>
2024-08-21 20:53:40 -0400

    add

--- settings.js
+++ settings.js
@@ -6,7 +6,7 @@ global.countrycode = "ID"; // Código de país - https://countrycode.org/ (CÓDI
 global.countrycodephone = "58"; // Teléfono del país - https://countrycode.org/ (CÓDIGO DEL PAÍS)
 global.timezone = "America/Caracas"; // Huso horario
 global.usePairingNumber = false; // true = Emparejamiento Code / false = QRCode
-global.pairingNumber = "4129056224"; // Número de WhatsApp utilizado como bot para emparejar números.
+global.pairingNumber = "1234567890"; // Número de WhatsApp utilizado como bot para emparejar números.
 //========================================================
 
 global.pp_bot = fs.readFileSync("./image/logo.png"); // Ubicación y nombre del logotipo
@@ -14,7 +14,7 @@ global.use_pp = true; // use a logo?
 
 //========================================================
 
-global.kontakadmin = ["4129056224"]; // Número de WhatsApp del administrador
+global.kontakadmin = ["1234567890"]; // Número de WhatsApp del administrador
 global.kirimkontak_admin = false; // true = Enviar automáticamente el contacto del administrador
 
 //========================================================
